47a65a74385930c0abcf294c65cfb42653447541
Петр Ломаев <petrucho060395@gmail.com>
2024-07-09 21:41:34 +0400

    step11 try2 rollbar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,11 +5,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider, ErrorBoundary } from '@rollbar/react';
+import Rollbar from 'rollbar';
 
-const rollbarConfig = {
+const rollbarInit = {
   accessToken: 'e985afdb5a7842339bfbbdb802503b0b',
   environment: 'testenv',
 }
+
+const rollbarConfig = new Rollbar(rollbarInit);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

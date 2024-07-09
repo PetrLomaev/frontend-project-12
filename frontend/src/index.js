ca0dddae2815a68ec0b98a0e4f41b3e42cfdf817
Петр Ломаев <petrucho060395@gmail.com>
2024-07-09 21:37:24 +0400

    step11 try rollbar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider, ErrorBoundary } from '@rollbar/react';
+
+const rollbarConfig = {
+  accessToken: 'e985afdb5a7842339bfbbdb802503b0b',
+  environment: 'testenv',
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider config={rollbarConfig}>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </Provider>
   </React.StrictMode>,
 );
